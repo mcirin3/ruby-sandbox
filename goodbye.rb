5b2fde2b2a3a7441fcc2e120f45cd9fc793216d5
Mark Cirineo <118200778+mcirin3@users.noreply.github.com>
2025-09-08 02:25:05 +0000

    new files to mess around with

---
+++ goodbye.rb
@@ -0,0 +1,1 @@
+pp "This is a message from the goodbye.rb!"
---
+++ hello.rb
@@ -0,0 +1,9 @@
+#here's a comment 
+
+my_string = "Hello from the github codespace!" 
+input_string = gets.chomp
+
+pp my_string
+pp input_string
+
+require "./goodbye.rb"
